dab659d7f380aaae7b7b25fb42ccdb76b292ccd0
Shine0417 <b07902109@ntu.edu.tw>
2021-06-16 04:13:57 +0800

    add crypto

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # VaccineSearchApp
+* Deploy Method (Be Careful)
+`gcloud app deploy --project causal-flame-316510 app.yaml ./frontend/frontend.yaml ./backend/backend.yaml`
+
+* Log
+service 有 frontend, backend
+```
+gcloud app logs tail -s <service>
+gcloud app logs -s <service>
+```
+* 查詢mongo DB 資料
+在gcp
+```
+mongo <IP> //連線
+
+show dbs //顯示db
+
+use test //使用db
+
+db.usermodels.find() //query 空的就代表找全部
+
+db.usermodels.find({name: "Shine"}) //找名字為Shine的usermodel
+```
--- backend/src/route.js
+++ backend/src/route.js
@@ -1,12 +1,27 @@
 import express from "express";
 import UserModel from "./model/UserModel.js";
 import HospitalModel from "./model/HospitalModel.js";
-
+import crypto from 'crypto'
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   res.send("Hello from Vaccine Searching Machine!!!");
 });
+
+const validateSignature = (publicKey, entryData) => {
+  if (publicKey == null)
+    return false
+  const signature = entryData.signature;
+  delete entryData["signature"]
+
+  const verify = crypto.createVerify('SHA256');
+  verify.write(JSON.stringify(entryData));
+  verify.end();
+  const messageIsValid = verify.verify(publicKey, signature);
+  // const messageIsValid = verify('sha256', JSON.stringify(entryData), publicKey, signature);
+  console.log("Signature verification: ", messageIsValid);
+  return messageIsValid
+}
 
 const validateVaccine = (vaccine) => {
   if (!Object.values(vaccine).some((x) => x == null || x == ""))
@@ -23,7 +38,6 @@ router.get("/loginUser", async (req, res) => {
     res.send({ data: "Success" });
   else
     res.send({ data: "Failed" });
-
 });
 
 router.get("/userData", async (req, res) => {
@@ -48,13 +62,15 @@ router.get("/addEntry", async (req, res) => {
     count: entryData.count,
     hospitalName: entryData.hospital_name,
     doctorName: entryData.doctor_name,
+    signature: entryData.signature,
   }
   console.log(vaccineData)
   const user = await UserModel.findOne({ ID: id, name: usr_name });
-  const vaccine = validateVaccine(vaccineData)
-  if (vaccine != null) {
+  const vaccine = validateVaccine(vaccineData);
+  const hospital = await HospitalModel.findOne({ name: vaccineData.hospitalName })
+  if (vaccine != null && hospital && validateSignature(hospital.publicKey, entryData)) {
     if (user != null) {
-      user.Injection.push(vaccine_name);
+      user.Injection.push(vaccine);
       await user.save();
     }
     else {
@@ -71,13 +87,34 @@ router.get("/loginHospital", async (req, res) => {
   const account = await HospitalModel.findOne({ name: req.query.hospital_name });
 
   if (account && account.validPassword(req.query.pwd)) {
-    res.send({ data: "Success" });
+    if (account.publicKey != null) {
+      res.send({ data: "Success" });
+    }
+    else {
+      res.send({ data: "Public Key Empty" })
+    }
     account.save();
   }
   else
     res.send({ data: "Failed" });
 });
 
+router.post("/setPublicKey", async (res, req) => {
+  let { hospital_name, pwd, public_key } = res.body.data;
+
+  const account = await HospitalModel.findOne({ name: hospital_name });
+  if (account && account.validPassword(pwd) && public_key != null) {
+    account.publicKey = public_key;
+    await account.save()
+    res.send("Success")
+  }
+  res.send("Failed")
+})
+
+router.get("/addHospital", async (req, res) => {
+  await generateHospitalAccount(req.query.name, req.query.password)
+  res.send("OK")
+});
 const generateHospitalAccount = async (name, password) => {
   const a = new HospitalModel({ name: name });
   a.password = a.generateHash(password);
